b98e155ddc2c2489e996f6564952a67d82dd8060
Erkut Anıl Dinç <erkutanil.dinc@gmail.com>
2024-07-20 18:27:00 +0300

    Config Server, Discovery Server and Customer Service have been added

---
+++ services/customer/src/main/java/com/anilerkut/ecommerce/handler/GlobalExcepitonHandler.java
@@ -0,0 +1,31 @@
+package com.anilerkut.ecommerce.handler;
+
+import com.anilerkut.ecommerce.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class GlobalExcepitonHandler {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException exp){
+
+        var errors = new HashMap<String,String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError)error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName,errorMessage);
+                });
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errors));
+    }
+}
